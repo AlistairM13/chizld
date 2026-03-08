d4e980cac2a862bde380140f62c086ac377e48a2
Alistair <alistairm2022@gmail.com>
2026-03-08 15:28:09 +0530

    feat(02-03): integrate HUD bars into CharacterScreen
    - Import HudBarTop and HudBarBottom components
    - Use useZoneStats hook for active zone count
    - HUD bars render as absolute positioned overlay
    - Typewriter animation triggers on screen mount

--- src/screens/character/CharacterScreen.tsx
+++ src/screens/character/CharacterScreen.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { colors } from '../../constants/colors';
 import { BodyCanvas } from '../../components/character/BodyCanvas';
+import { HudBarTop, HudBarBottom } from '../../components/character/HudBar';
+import { useZoneStats } from '../../hooks/useZoneStats';
 
 export function CharacterScreen() {
+  const { warmCount } = useZoneStats();
+
   return (
     <View style={styles.container}>
       <BodyCanvas />
-      {/* Zone cards and HUD bars will be added in plans 02-02 and 02-03 */}
+
+      {/* Zone cards will be added in plan 02-02 */}
+
+      {/* HUD bars - absolute positioned overlay */}
+      <HudBarTop />
+      <HudBarBottom activeZoneCount={warmCount} totalZones={8} />
     </View>
   );
 }
